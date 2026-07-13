eed2fc5552b8b5d7d64bca6eb715e9bbe71c60fc
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-07-13 23:37:08 +0100

    update user profile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
 
        User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@gmail.com',            
+            'username' => 'testuser' ,
+            'email' => 'test@gmail.com',                        
        ]);
 
         $categories = [
