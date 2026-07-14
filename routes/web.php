7a89d7da14b9acb37490fe1b0f08e53e2be5797a
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-07-14 19:51:54 +0100

    make some routes public

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/@{user:username}' , [PublicProfileController::class , 'show'])->name('profile.show');
 
+Route::get('/', [PostController::class , 'index'])
+    ->name('dashboard');
+Route::get('/@{username}/{post:slug}' , [PostController::class , 'show'])
+    ->name('post.show');
+
 
 Route::middleware(['auth' , 'verified'])->group(function(){
-    Route::get('/', [PostController::class , 'index'])
-    ->name('dashboard');
 
     Route::get('/category/{category}' , [PostController::class , 'category'])
     ->name('post.byCategory');
@@ -27,8 +30,7 @@ Route::middleware(['auth' , 'verified'])->group(function(){
     Route::post('/post/create' , [PostController::class , 'store'])
     ->name('post.store');
 
-    Route::get('/@{username}/{post:slug}' , [PostController::class , 'show'])
-    ->name('post.show');
+  
 
     Route::post('/follow/{user}' , [FollowerController::class , 'followToggle'])
     ->name('follow');
