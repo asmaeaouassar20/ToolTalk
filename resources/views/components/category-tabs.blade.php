89e01f36df263fb430ff5e5a48b72c5786079c72
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-07-16 00:06:00 +0100

    fix hover on category

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -3,7 +3,7 @@
 
                         <li class="w-full focus-within:z-10">
                             <a href="/"
-                                class="{{ request('category') ? '' : 'bg-gray-600 text-white' }} inline-block w-full h-full flex items-center justify-center text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-4 focus:ring-neutral-secondary-strong font-medium leading-5 text-sm px-4 py-4 focus:outline-none">
+                                class="{{ request('category') ? '' : 'bg-gray-600 text-white' }} w-full h-full flex items-center justify-center text-body bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-heading focus:ring-4 focus:ring-neutral-secondary-strong font-medium leading-5 text-sm px-4 py-4 focus:outline-none">
                                 all
                             </a>
                         </li>
@@ -12,7 +12,7 @@
                             <li class="w-full  focus-within:z-10">
                                 <a href="{{ route('post.byCategory' , $category) }}"
 
-                                    class="{{ Route::currentRouteNamed('post.byCategory') && request('category')->id ==$category->id ? 'bg-gray-600 text-white' : ''  }} flex items-center justify-center inline-block w-full  h-full  text-body  border border-default rounded-e-base focus:ring-4 focus:ring-neutral-secondary-strong font-medium leading-5 text-sm px-4 py-4 ">
+                                    class="{{ Route::currentRouteNamed('post.byCategory')  && request('category') && request('category')->id ==$category->id ? 'bg-gray-600 text-white' : ''  }} flex items-center justify-center w-full  h-full  text-body  border border-default rounded-e-base focus:ring-4 focus:ring-neutral-secondary-strong font-medium leading-5 text-sm px-4 py-4 hover:bg-gray-200 hover:text-gray-900">
                                     {{ $category->name }}
                                 </a>
                             </li>
